24e3a223b6e54b6b5518bc9bd3f69c522a5392d6
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-09-17 20:30:01 +0600

    finish login design and user validation

---
+++ src/Pages/SignIn.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useForm } from "react-hook-form"
+const SignIn = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => console.log(data)
+  return (
+    <>
+      <div className='container mx-auto px-[10px]'>
+        <div className='h-screen flex justify-center items-center'>
+            <div className='p-[40px] border w-[60%] rounded-lg'>
+                <h2 className='text-2xl font-[600] text-center'>Login Here</h2>
+                <p className='text-center mt-[10px]'>Login And Explore More</p>
+            <form onSubmit={handleSubmit(onSubmit)} className='mt-[20px]'>
+                <div>
+                    <label htmlFor="email">Enter Your Email</label>
+                    <input {...register("email", { required: true })} className='w-full border focus:outline-none p-[10px] rounded-lg' type="email" placeholder='Enter Your Email' />
+                    {errors.email && <span className='text-red-500'>This field is required</span>}
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input {...register("password", { required: true })} className='w-full border focus:outline-none p-[10px] rounded-lg' type="password" placeholder='Enter Password' />
+                    {errors.password && <span className='text-red-500'>This field is required</span>}
+                </div>
+                <div className='mt-[20px]'>
+                    <button className='w-full border focus:outline-none p-[10px] rounded-lg'>Login</button>
+                </div>
+            </form>
+            </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SignIn
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,16 +1,22 @@
+
 import Regtister from "../Pages/Regtister";
+import SignIn from "../Pages/SignIn";
 
 const { createBrowserRouter } = require("react-router-dom");
 const { default: Home } = require("../Pages/Home");
 
 const router = createBrowserRouter([
     {
-        path : '/',
-        element : <Home></Home>
+        path: '/',
+        element: <Home></Home>
     },
     {
-        path : "/register",
-        element : <Regtister></Regtister>
+        path: "/register",
+        element: <Regtister></Regtister>
+    },
+    {
+        path: "/signin",
+        element: <SignIn></SignIn>
     }
 ])
 
