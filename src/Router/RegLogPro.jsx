94d360b674ae74c59fb864b9be5c8c3084aa6cdd
Ruhit Baidya <ruhitbaidya01@gmail.com>
2024-10-15 22:15:11 +0600

    finish some work

--- src/Router/RegLogPro.jsx
+++ src/Router/RegLogPro.jsx
@@ -3,14 +3,13 @@ import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const RegLogPro = () => {
-    const [loading, setLoading] = useState(true);
+ 
     const token = localStorage.getItem('token');
     const userLocate = useLocation();
     console.log(userLocate.pathname)
     const navigate = useNavigate();
     useEffect(()=>{
         if(token){
-            setLoading(false)
           return navigate('/')
         }else{
           return navigate(userLocate?.pathname)
